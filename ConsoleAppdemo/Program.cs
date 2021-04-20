d3fb0e8be83ea23b1c76138f5c0f3a404a93af22
Pedro Hernández <pedrohernandez@gmail.com>
2021-04-20 20:58:45 +0200

    cambios

--- ConsoleAppdemo/Program.cs
+++ ConsoleAppdemo/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
